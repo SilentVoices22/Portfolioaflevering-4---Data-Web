a2dcfcab8fdc65a3a88d5801adfbef9bbb138abe
gume0002 <gmeyer171@gmail.com>
2025-11-08 17:22:10 +0100

    .

--- chartCode.js
+++ chartCode.js
@@ -8,3 +8,27 @@ const chart = new Chart(ctx, {
         }]
     }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const ctx = document.querySelector('#chart').getContext('2d');
+const chart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            data: [12, 19, 3, 5, 2, 3],
+        }]
+    }
+});
