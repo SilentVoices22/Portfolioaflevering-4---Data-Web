02467352ba6f6d37413c2f494a516b8efb34348a
SilentVoices22 <pinyuanlu@gmail.com>
2025-11-10 11:16:04 +0100

    adding options

--- chartCode.js
+++ chartCode.js
@@ -3,10 +3,18 @@ const ctx = document.querySelector('#chart').getContext('2d');
 const chartForMonths = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Januar', 'Februar', 'Marts', 'April', 'Maj', 'Juni','Juli','August', 'September','Oktober', 'November','December'],
+        labels: ['Januar', 'Februar', 'Marts', 'April', 'Maj', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'December'],
         datasets: [{
             label: 'Monthly Invoice',
-            data: [34,33,35,33,35,35,35,35,33,35,34,35],
+            data: [34, 33, 35, 33, 35, 35, 35, 35, 33, 35, 34, 35],
+
         }]
+
+    }, options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
     }
 });
