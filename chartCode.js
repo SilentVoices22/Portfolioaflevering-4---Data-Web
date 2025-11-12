7c537f4bd31c660e76963c048ccccaa5ec0dcbc0
SilentVoices22 <pinyuanlu@gmail.com>
2025-11-12 13:53:43 +0100

    Refactor chart type to line and update dataset labels; enhance HTML structure with navigation and styling improvements

--- chartCode.js
+++ chartCode.js
@@ -1,13 +1,23 @@
 const ctx = document.querySelector('#chart').getContext('2d');
 
 const chartForMonths = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
         labels: ['Januar', 'Februar', 'Marts', 'April', 'Maj', 'Juni','Juli','August', 'September','Oktober', 'November','December'],
         datasets: [{
+               label: 'Årlig invoice per måned',
             data: [34,33,35,33,35,35,35,35,33,35,34,35],
+            fill: true, 
         }]
+    }, options: {
+    scales: {
+        y: {
+            
+            suggestedMin: 32,
+            suggestedMax: 36 
+        }
     }
+}
 });
 
 
@@ -19,6 +29,7 @@ const chartForGenres = new Chart(ctx2, {
         labels: ['Rock', 'Latin', 'Reggae', 'Metal', 'Alternative & Punk', 'Soundtrack','Classical','Pop', 'Blues','R&B/Soul', 'Hiphop/Rap','World', "Sci Fi & Fantasy"],
         datasets: [{
             data: [691,502,83,74,71,55,39,36,31,31,30,27,27,8],
+            
         }]
     }
 });
