0d97ecec4343f665d7e10ec0003f6bbe2ff2778c
gume0002 <gmeyer171@gmail.com>
2025-11-12 14:55:01 +0100

    opdateret css og html

--- chartCode.js
+++ chartCode.js
@@ -5,7 +5,7 @@ const chartForMonths = new Chart(ctx, {
     data: {
         labels: ['Januar', 'Februar', 'Marts', 'April', 'Maj', 'Juni','Juli','August', 'September','Oktober', 'November','December'],
         datasets: [{
-               label: 'Årlig invoice per måned',
+               label: 'Invoice pr. måned',
             data: [34,33,35,33,35,35,35,35,33,35,34,35],
             fill: true, 
         }]
@@ -28,6 +28,7 @@ const chartForGenres = new Chart(ctx2, {
     data: {
         labels: ['Rock', 'Latin', 'Reggae', 'Metal', 'Alternative & Punk', 'Soundtrack','Classical','Pop', 'Blues','R&B/Soul', 'Hiphop/Rap','World', "Sci Fi & Fantasy"],
         datasets: [{
+            label: 'Popularitet for genre i Brasillien',
             data: [691,502,83,74,71,55,39,36,31,31,30,27,27,8],
             
         }]
